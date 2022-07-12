07aaae0e4e6034bd331ade7761ed12a35d2bd2e9
Nushanaruna <108817263+Nushanaruna@users.noreply.github.com>
2022-07-12 09:06:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-    VERSION: 'V 1.0.0 PUBLIC',
+    VERSION: 'V 1.0.0 PRIVATE',
     MENU: process.env.MENU_CMD=== undefined ? 'menu' : process.env.MENU_CMD,
     SESSION: process.env.SESSION_NAME === undefined ? 'session' : process.env.SESSION_NAME,
-    OWN_NUMBER: process.env.OWNER_NUMBER === undefined ? '94729352830' : process.env.OWN_NUMBER,
-    OWN_NAME: process.env.OWNER_NAME === undefined ? 'ШHłТΞ HΛϾКΞЯS' : process.env.OWN_NAME,
-    BOT_NAME: process.env.BOT_NAME === undefined ? '[🇱🇰𝚱𝚰𝚴Ｇ 𝛃𝚯𝚪🤘] - ᎷᎠ' : process.env.BOT_NAME,
-    ST_PKG: process.env.STICKER_PACKAGE === undefined ? '94729352830' : process.env.ST_PKG,
+    OWN_NUMBER: process.env.OWNER_NUMBER === undefined ? '94741089734' : process.env.OWN_NUMBER,
+    OWN_NAME: process.env.OWNER_NAME === undefined ? 'WHITE CRAZY' : process.env.OWN_NAME,
+    BOT_NAME: process.env.BOT_NAME === undefined ? 'CRAZY BOT-ᎷᎠ' : process.env.BOT_NAME,
+    ST_PKG: process.env.STICKER_PACKAGE === undefined ? '94741089734' : process.env.ST_PKG,
     SD: process.env.SONG_DOWNLOAD === undefined ? 's d' : process.env.SONG_DOWNLOAD,
     SU: process.env.SONG_UPLOAD === undefined ? 's u' : process.env.SONG_UPLOAD,
     VD: process.env.VIDEO_DOWNLOAD === undefined ? 'v d' : process.env.VIDEO_DOWNLOAD,
